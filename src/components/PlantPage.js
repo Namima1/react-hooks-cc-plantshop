6e903bb08d3f164eedd62a5c53685b78ff75e631
Namima Islam <namimaislam308@gmail.com>
2024-10-16 23:10:25 -0400

    completed lab

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  const handleSearch = (query) => setSearchQuery(query.toLowerCase());
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchQuery)
+  );
+
+  const addNewPlant = (newPlant) => setPlants([...plants, newPlant]);
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={addNewPlant} />
+      <Search onSearch={handleSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
